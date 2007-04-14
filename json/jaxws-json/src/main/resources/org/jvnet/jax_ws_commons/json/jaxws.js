a49a2d62f9af5223633ed78c3136e43e5d56c149
Kohsuke Kawaguchi <kohsuke@java.net>
2007-04-14 21:29:59 +0000

    we need JSON encoder.
    svn path=/trunk/; revision=223

--- json/jaxws-json/src/main/resources/org/jvnet/jax_ws_commons/json/jaxws.js
+++ json/jaxws-json/src/main/resources/org/jvnet/jax_ws_commons/json/jaxws.js
@@ -1,5 +1,5 @@
 
-    init : function () {
+    createXmlHttpRequest : function () {
         if (window.XMLHttpRequest) {
             return new XMLHttpRequest();
         } else if (window.ActiveXObject) {
@@ -9,13 +9,41 @@
         }
     },
 
+    /*  Prototype JavaScript framework, version 1.5.1_rc2
+     *  (c) 2005-2007 Sam Stephenson
+     *
+     *  Prototype is freely distributable under the terms of an MIT-style license.
+     *  For details, see the Prototype web site: http://www.prototypejs.org/
+     *
+    /*--------------------------------------------------------------------------*/
+    toJSON: function(object) {
+        var type = typeof object;
+        switch (type) {
+        case 'undefined':
+        case 'function':
+        case 'unknown': return;
+        case 'object': break;
+        default: return object.toString();
+        }
+        if (object === null) return 'null';
+        if (object.ownerDocument === document) return;
+        var results = [];
+        for (var property in object) {
+            var value = this.toJSON(object[property]);
+            if (value !== undefined)
+                results.push(property + ':' + value);
+        }
+        return '{' + results.join(',') + '}';
+    },
+
+
     post : function(obj, func) {
-        var req = this.init();
+        var req = this.createXmlHttpRequest();
         req.onreadystatechange = function() {
             if (req.readyState == 4) {
                 if (req.status == 200) {
                     // unwarp
-                    var rsp = eval(req.responseText);
+                    var rsp = eval('('+req.responseText+')');
                     for( p in rsp ) {
                         if(rsp.hasOwnProperty(p)) {
                             rsp = rsp[p];
@@ -30,6 +58,6 @@
         };
         req.open("POST", this.url, true);
         req.setRequestHeader("Content-Type", "application/json");
-        req.send(obj);
+        req.send(this.toJSON(obj));
     },
 
