33663d150cdbe0b47c7c66852baef2653a035b53
Kohsuke Kawaguchi <kohsuke@java.net>
2007-04-14 19:49:10 +0000

    printf is better for this kind of formatting purpose.
    svn path=/trunk/; revision=215

--- json/jaxws-json/src/main/java/org/jvnet/jax_ws_commons/json/ClientGenerator.java
+++ json/jaxws-json/src/main/java/org/jvnet/jax_ws_commons/json/ClientGenerator.java
@@ -3,10 +3,10 @@ package org.jvnet.jax_ws_commons.json;
 import com.sun.xml.ws.api.model.JavaMethod;
 import com.sun.xml.ws.api.model.SEIModel;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
 import java.util.Iterator;
 
 /**
@@ -21,8 +21,7 @@ final class ClientGenerator {
         this.model = model;
     }
 
-    void generate() throws IOException {
-        PrintStream os = new PrintStream(new FileOutputStream("jaxws.js"));
+    void generate(PrintWriter os) throws IOException {
         writeGlobal(os);
         writeStatic(os);
         writeOperations(os);
@@ -31,60 +30,54 @@ final class ClientGenerator {
     }
 
     // TODO: need to declare URL as global variable
-    private void writeGlobal(PrintStream os) throws IOException {
+    private void writeGlobal(PrintWriter os) throws IOException {
         String serviceName = model.getServiceQName().getLocalPart();
-        os.append("var ");
-        os.append(serviceName);
-        os.append(" = {\n");
+        os.printf("var %s = {\n",serviceName);
         shift(os);
-        os.append("url : \"TODO\",\n");
+        os.println("url : \"TODO\",\n");
     }
 
-    private void writeStatic(PrintStream os) throws IOException {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("jaxws.js");
-        int ch;
-        while((ch = is.read()) != -1) {
-            os.write(ch);
+    private void writeStatic(PrintWriter os) throws IOException {
+        Reader is = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("jaxws.js"));
+        char[] buf = new char[256];
+        int len;
+        while((len = is.read(buf)) != -1) {
+            os.write(buf,0,len);
         }
         is.close();
     }
 
-    private void writeOperations(PrintStream os) {
+    private void writeOperations(PrintWriter os) {
         Iterator<? extends JavaMethod> it = model.getJavaMethods().iterator();
         while(it.hasNext()) {
             writeOperation(it.next(), it.hasNext(), os);
         }
     }
 
-    private void writeOperation(JavaMethod jm, boolean next, PrintStream os) {
+    private void writeOperation(JavaMethod jm, boolean next, PrintWriter os) {
         String reqName = jm.getPayloadName().getLocalPart();
         String methodName = jm.getMethod().getName();
         String resName = "getResponse"; // TODO
 
         shift(os);
-        os.append(methodName);
-        os.append(" : function(obj, callback) {\n");
+        os.printf("%s : function(obj, callback) {\n",methodName);
         shift2(os);
-        os.append("post({");
-        os.append(reqName);
-        os.append(" : obj}, function(obj) { callback(obj.");
-        os.append(resName);
-        os.append("); });\n");
+        os.printf("post({%s:obj}, function(obj) { callback(obj.%s); });\n", reqName,resName);
         shift(os);
         if (next) { os.append("},\n\n"); } else { os.append("}\n\n"); }
     }
 
-    private static void shift(PrintStream os) {
+    private static void shift(PrintWriter os) {
         os.append("    ");
     }
 
-    private static void shift2(PrintStream os) {
+    private static void shift2(PrintWriter os) {
         shift(os);
         shift(os);
     }
 
-    private void writeClosure(PrintStream os) {
-        os.append("};\n");
+    private void writeClosure(PrintWriter os) {
+        os.println("};");
     }
     
 }
