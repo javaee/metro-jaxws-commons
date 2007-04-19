461e0c248e7fd50559009ed6a9b9d378203a0545
jitendra kotamraju <jitu@java.net>
2007-04-19 22:28:31 +0000

    Crude version to encode and decode messages for SMTP transport.
    svn path=/trunk/; revision=263

--- transport/smtp/jaxws-smtp/src/main/java/org/jvnet/jax_ws_commons/transport/smtp/server/SMTPAdapter.java
+++ transport/smtp/jaxws-smtp/src/main/java/org/jvnet/jax_ws_commons/transport/smtp/server/SMTPAdapter.java
@@ -3,18 +3,23 @@ package org.jvnet.jax_ws_commons.transport.smtp.server;
 import com.sun.xml.ws.api.server.Adapter;
 import com.sun.xml.ws.api.server.WSEndpoint;
 import com.sun.xml.ws.api.server.TransportBackChannel;
+import com.sun.xml.ws.api.server.WebServiceContextDelegate;
 import com.sun.xml.ws.api.message.Packet;
 import com.sun.xml.ws.api.message.Message;
 import com.sun.xml.ws.api.message.ExceptionHasMessage;
 import com.sun.xml.ws.api.pipe.Codec;
 import com.sun.xml.ws.api.pipe.ContentType;
+import com.sun.xml.ws.api.pipe.Fiber;
 import com.sun.xml.ws.transport.http.WSHTTPConnection;
 import com.sun.xml.ws.util.ByteArrayBuffer;
 import com.sun.xml.ws.server.UnsupportedMediaException;
 import com.sun.xml.ws.server.ServerRtException;
 import com.sun.istack.NotNull;
+import com.sun.istack.Nullable;
 
 import javax.xml.ws.WebServiceException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -23,6 +28,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.List;
 import java.util.Map;
+import java.util.Enumeration;
+import java.security.Principal;
+
+import org.jvnet.jax_ws_commons.transport.smtp.mail.EmailEndpoint;
+import org.jvnet.jax_ws_commons.transport.smtp.mail.MailHandler;
 
 /**
  * @author Jitendra Kotamraju
@@ -43,8 +53,18 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
 
     private static final Logger LOGGER = Logger.getLogger(SMTPAdapter.class.getName());
 
+    private EmailEndpoint email;
+
     SMTPAdapter(WSEndpoint endpoint) {
         super(endpoint);
+    }
+
+    public void setEmailEndpoint(EmailEndpoint email) {
+        this.email = email;
+    }
+
+    public EmailEndpoint getEmailEndpoint() {
+        return email;
     }
 
     protected SMTPToolkit createToolkit() {
@@ -60,33 +80,55 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
       *         ExceptionHasMessage exception that contains particular fault message
       *         UnsupportedMediaException to indicate to send 415 error code
       */
-     private Packet decodePacket(@NotNull WSHTTPConnection con, @NotNull Codec codec) throws IOException {
-         String ct = con.getRequestHeader("Content-Type");
-         InputStream in = con.getInput();
+     private Packet decodePacket(@NotNull MimeMessage con, @NotNull Codec codec) throws MessagingException, IOException {
+         String ct = con.getContentType();
+         InputStream in = con.getInputStream();
          Packet packet = new Packet();
-         packet.soapAction = con.getRequestHeader("SOAPAction");
-         packet.wasTransportSecure = con.isSecure();
-         packet.acceptableMimeTypes = con.getRequestHeader("Accept");
-         packet.addSatellite(con);
-         packet.transportBackChannel = new Oneway(con);
-         packet.webServiceContextDelegate = con.getWebServiceContextDelegate();
+         packet.soapAction = con.getHeader("SOAPAction")[0];
+         packet.wasTransportSecure = false;
+         packet.acceptableMimeTypes = con.getHeader("Accept")[0];
+         //packet.addSatellite(con);        TODO
+         //packet.transportBackChannel = new Oneway(con);
+         packet.webServiceContextDelegate = new WebServiceContextDelegate() {
+
+             public Principal getUserPrincipal(@NotNull Packet request) {
+                 return null;
+             }
+
+             public boolean isUserInRole(@NotNull Packet request, String role) {
+                 return false;
+             }
+
+             @NotNull
+             public String getEPRAddress(@NotNull Packet request, @NotNull WSEndpoint endpoint) {
+                 return null;
+             }
+
+             @Nullable
+             public String getWSDLAddress(@NotNull Packet request, @NotNull WSEndpoint endpoint) {
+                 return null;
+             }
+         };
 
          if (dump) {
              ByteArrayBuffer buf = new ByteArrayBuffer();
              buf.write(in);
-             dump(buf, "SMTP request", con.getRequestHeaders());
+             dump(buf, "SMTP request", con.getAllHeaders());
              in = buf.newInputStream();
          }
          codec.decode(in, ct, packet);
          return packet;
      }
 
-     private void encodePacket(@NotNull Packet packet, @NotNull WSHTTPConnection con, @NotNull Codec codec) throws IOException {
+     private void encodePacket(@NotNull Packet packet, @NotNull Codec codec) throws MessagingException, IOException {
+/*
          if (con.isClosed()) {
              return;                 // Connection is already closed
          }
+*/
          Message responseMessage = packet.getMessage();
          if (responseMessage == null) {
+/*
              if (!con.isClosed()) {
                  // set the response code if not already set
                  // for example, 415 may have been set earlier for Unsupported Content-Type
@@ -99,43 +141,50 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
                      throw new WebServiceException(e);
                  }
              }
+*/
          } else {
-             if (con.getStatus() == 0) {
+/*             if (con.getStatus() == 0) {
                  // if the appliation didn't set the status code,
                  // set the default one.
                  con.setStatus(responseMessage.isFault()
                          ? HttpURLConnection.HTTP_INTERNAL_ERROR
                          : HttpURLConnection.HTTP_OK);
-             }
+             }*/
 
              ContentType contentType = codec.getStaticContentType(packet);
              if (contentType != null) {
-                 con.setContentTypeResponseHeader(contentType.getContentType());
+/*                 con.setContentTypeResponseHeader(contentType.getContentType());
                  OutputStream os = con.getOutput();
                  if (dump) {
                      ByteArrayBuffer buf = new ByteArrayBuffer();
                      codec.encode(packet, buf);
-                     dump(buf, "SMTP response " + con.getStatus(), con.getResponseHeaders());
+                     //dump(buf, "SMTP response " + con.getStatus(), con.getResponseHeaders());
                      buf.writeTo(os);
                  } else {
                      codec.encode(packet, os);
                  }
-                 os.close();
-             } else {
+                 os.close();*/
+
                  ByteArrayBuffer buf = new ByteArrayBuffer();
+                 codec.encode(packet, buf);
+                 MimeMessage msg = new MimeMessage(email.getSession());
+                 msg.setContent(new String(buf.toByteArray()), contentType.getContentType());
+                 email.send(msg);
+             } else {
+/*                 ByteArrayBuffer buf = new ByteArrayBuffer();
                  contentType = codec.encode(packet, buf);
                  con.setContentTypeResponseHeader(contentType.getContentType());
                  if (dump) {
-                     dump(buf, "HTTP response " + con.getStatus(), con.getResponseHeaders());
+                     //dump(buf, "HTTP response " + con.getStatus(), con.getResponseHeaders());
                  }
                  OutputStream os = con.getOutput();
                  buf.writeTo(os);
-                 os.close();
+                 os.close();*/
              }
          }
      }
 
-     public void invokeAsync(final WSHTTPConnection con) throws IOException {
+     public void invokeAsync(final MimeMessage con) throws IOException {
          final SMTPToolkit tk = pool.take();
          final Packet request;
          try {
@@ -144,17 +193,24 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
              LOGGER.log(Level.SEVERE, e.getMessage(), e);
              Packet response = new Packet();
              response.setMessage(e.getFaultMessage());
-             encodePacket(response, con, tk.codec);
+             try {
+                encodePacket(response, tk.codec);
+             } catch(MessagingException me) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+             }
              pool.recycle(tk);
-             con.close();
+             //con.close();
              return;
          } catch(UnsupportedMediaException e) {
              LOGGER.log(Level.SEVERE, e.getMessage(), e);
              Packet response = new Packet();
-             con.setStatus(WSHTTPConnection.UNSUPPORTED_MEDIA);
-             encodePacket(response, con, tk.codec);
+             //con.setStatus(WSHTTPConnection.UNSUPPORTED_MEDIA);
+             //encodePacket(response, tk.codec);
              pool.recycle(tk);
-             con.close();
+             //con.close();
+             return;
+         } catch(MessagingException e) {
+             LOGGER.log(Level.SEVERE, e.getMessage(), e);
              return;
          }
 
@@ -162,13 +218,15 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
              public void onCompletion(@NotNull Packet response) {
                  try {
                      try {
-                         encodePacket(response, con, tk.codec);
+                         encodePacket(response, tk.codec);
                      } catch(IOException ioe) {
                          LOGGER.log(Level.SEVERE, ioe.getMessage(), ioe);
+                     } catch(MessagingException me) {
+                        LOGGER.log(Level.SEVERE, me.getMessage(), me);
                      }
                      pool.recycle(tk);
                  } finally{
-                     con.close();
+                     //con.close();
                  }
              }
          });
@@ -196,7 +254,29 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
         }
     }
 
-    private void dump(ByteArrayBuffer buf, String caption, Map<String, List<String>> headers) throws IOException {
+    public class SmtpHandler implements MailHandler {
+        final Packet request;
+
+        SmtpHandler(Packet request) {
+            this.request = request;
+        }
+
+        public void onNewMail(MimeMessage message) {
+            try {
+                invokeAsync(message);
+            } catch(IOException ioe) {
+                LOGGER.log(Level.SEVERE, ioe.getMessage(), ioe);
+            }
+        }
+
+        public boolean isClosed() {
+            return false;
+        }
+    }
+
+
+    private void dump(ByteArrayBuffer buf, String caption, Enumeration headers) throws IOException {
+        /*
         System.out.println("---["+caption +"]---");
         if (headers != null) {
             for (Map.Entry<String, List<String>> header : headers.entrySet()) {
@@ -213,6 +293,7 @@ public class SMTPAdapter extends Adapter<SMTPAdapter.SMTPToolkit> {
         }
         buf.writeTo(System.out);
         System.out.println("--------------------");
+        */
     }
 
 
