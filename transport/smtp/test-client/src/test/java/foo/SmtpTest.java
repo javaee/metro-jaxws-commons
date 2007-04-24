80d76b434f7c86a14771630f5bd9f08befa3f5af
jitendra kotamraju <jitu@java.net>
2007-04-24 23:34:19 +0000

    Updating the client's smtp test case
    svn path=/trunk/; revision=310

--- transport/smtp/test-client/src/test/java/foo/SmtpTest.java
+++ transport/smtp/test-client/src/test/java/foo/SmtpTest.java
@@ -1,6 +1,9 @@
 package foo;
 
 import junit.framework.TestCase;
+import java.util.Properties;
+import javax.xml.ws.BindingProvider;
+import org.jvnet.jax_ws_commons.transport.smtp.client.SmtpTransportTube;
 
 /**
  * @author Jitendra Kotamraju
@@ -16,5 +19,16 @@ public class SmtpTest extends TestCase {
     }
 
     public void testSmtp() {
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host","localhost");
+        props.put(SmtpTransportTube.class.getName()+".dump","true");
+
+        GreetingService proxy =
+            new GreetingServiceService().getGreetingServicePort();
+        ((BindingProvider)proxy).getRequestContext().put(
+            BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+            "smtp://smtp.transport.server@kohsuke.org?!pop3://smtp.transport.client:jaxws123@kohsuke.org/"
+        );
+        assertEquals("Bonjour, jitu", proxy.sayHelloTo("jitu"));
     }
 }
