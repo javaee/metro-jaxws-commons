a5650ce16db54e554babfd991e28836fd980d48e
Kohsuke Kawaguchi <kohsuke@java.net>
2007-04-25 23:54:30 +0000

    integrated mock-javamail for testing.
    svn path=/trunk/; revision=315

--- transport/smtp/jaxws-smtp/src/test/java/pop3/SmtpTest.java
+++ transport/smtp/jaxws-smtp/src/test/java/pop3/SmtpTest.java
@@ -42,25 +42,21 @@ public class SmtpTest
      */
     public void testApp() throws JAXBException, ClassNotFoundException {
         Properties props = System.getProperties();
-        props.put("mail.smtp.host","localhost");
-        //props.put("mail.smtp.port", "587");
-        //props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.starttls.enable","true");
         props.put(SmtpTransportTube.class.getName()+".dump","true");
         Service service = Service.create(new QName("FakeService"));
         service.getClass().getClassLoader().getResource("META-INF/services/com.sun.xml.ws.api.pipe.TransportTubeFactory");
 
-        String add = "smtp://smtp.transport.server@kohsuke.org?!pop3://smtp.transport.server:jaxws123@kohsuke.org/";
-        //String add = "smtp://smtp.transport:beat.net@gmail.com?mail.pop3.socketFactory.class=javax.net.ssl.SSLSocketFactory&mail.pop3.port=995!pop3s://smtp.transport:beat.net@pop.gmail.com/";
+        String add = "smtp://server@sun.com!pop3://server:password@sun.com/";
         service.addPort(new QName("FakePort"), SOAPBinding.SOAP11HTTP_BINDING, add);
+
         JAXBContext jaxbCtx = JAXBContext.newInstance(Book.class);
         Dispatch dispatch = service.createDispatch(new QName("FakePort"), jaxbCtx, Service.Mode.PAYLOAD);
         dispatch.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, add);
         Book resp = (Book) dispatch.invoke(new Book("Midnight's Children", "Salman Rushdie", "Unknown"));
+
         assertNotNull(resp);
         assertTrue(resp.getTitle().equals("Midnight's Children") &&
-            resp.getPublisher().equals("Unkwon") &&
+            resp.getPublisher().equals("Unknown") &&
             resp.getAuthor().equals("Salman Rushdie"));
-//        assertTrue(true);
     }
 }
