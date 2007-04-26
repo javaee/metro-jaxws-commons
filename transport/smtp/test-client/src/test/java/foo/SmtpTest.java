96a77267de7584a6806b120475f961b3d36c7867
jitendra kotamraju <jitu@java.net>
2007-04-26 01:57:43 +0000

    Using SMTPFeature to configure all the SMTP configuration.
    svn path=/trunk/; revision=317

--- transport/smtp/test-client/src/test/java/foo/SmtpTest.java
+++ transport/smtp/test-client/src/test/java/foo/SmtpTest.java
@@ -4,6 +4,9 @@ import junit.framework.TestCase;
 import java.util.Properties;
 import javax.xml.ws.BindingProvider;
 import org.jvnet.jax_ws_commons.transport.smtp.client.SmtpTransportTube;
+import org.jvnet.jax_ws_commons.transport.smtp.SMTPFeature;
+import org.jvnet.jax_ws_commons.transport.smtp.POP3Info;
+import org.jvnet.jax_ws_commons.transport.smtp.SenderInfo;
 
 /**
  * @author Jitendra Kotamraju
@@ -28,11 +31,17 @@ public class SmtpTest extends TestCase {
         props.put("mail.smtp.port","10025");
         props.put(SmtpTransportTube.class.getName()+".dump","true");
 
+        SMTPFeature feature = new SMTPFeature();
+        feature.setIncoming(new POP3Info("kohsuke.org", "smtp.transport.client",
+            "jaxws123"));
+        feature.setOutgoing(new SenderInfo("kohsuke.sfbay.sun.com", "10025",
+            "smtp.transport.client@kohsuke.org"));
         GreetingService proxy =
-            new GreetingServiceService().getGreetingServicePort();
+            new GreetingServiceService().getGreetingServicePort(feature);
+
         ((BindingProvider)proxy).getRequestContext().put(
             BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-            "smtp://smtp.transport.server@kohsuke.org?!pop3://smtp.transport.client:jaxws123@kohsuke.org/"
+            "smtp://smtp.transport.server@kohsuke.org"
         );
         assertEquals("Bonjour, jitu", proxy.sayHelloTo("jitu"));
     }
