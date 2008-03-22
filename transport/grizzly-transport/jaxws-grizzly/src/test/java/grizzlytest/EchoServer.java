8a9ce28b818dae608aa837f85e610428c5ac344f
jitendra kotamraju <jitu@java.net>
2008-03-22 00:50:14 +0000

    Updating the AsyncProvider test.
    svn path=/trunk/; revision=605

--- transport/grizzly-transport/jaxws-grizzly/src/test/java/grizzlytest/EchoServer.java
+++ transport/grizzly-transport/jaxws-grizzly/src/test/java/grizzlytest/EchoServer.java
@@ -1,13 +1,12 @@
 package grizzlytest;
 
-import javax.xml.transform.Source;
-import javax.xml.ws.Provider;
-import javax.xml.ws.WebServiceProvider;
-import javax.xml.ws.WebServiceContext;
-
 import com.sun.xml.ws.api.server.AsyncProvider;
 import com.sun.xml.ws.api.server.AsyncProviderCallback;
-import com.sun.xml.ws.api.server.AsyncProviderCallback;
+
+import javax.xml.transform.Source;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.WebServiceException;
+import javax.xml.ws.WebServiceProvider;
 
 /**
  * @author Jitendra Kotamraju
@@ -16,10 +15,34 @@ import com.sun.xml.ws.api.server.AsyncProviderCallback;
 public class EchoServer implements AsyncProvider<Source> {
 
     public void invoke(Source request, AsyncProviderCallback<Source> callback,
-        WebServiceContext ctxt) {
+                       WebServiceContext ctxt) {
 
-        System.out.println("Server invoked "+request);
-        callback.send(request);
+        System.out.println("Service is invoked " + request);
+        new Thread(new RequestHandler(callback, request)).start();
+    }
+
+    private static class RequestHandler implements Runnable {
+        final AsyncProviderCallback<Source> cbak;
+        final Source req;
+
+        public RequestHandler(AsyncProviderCallback<Source> cbak, Source req) {
+            this.cbak = cbak;
+            this.req = req;
+        }
+
+        public void run() {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ie) {
+                cbak.sendError(new WebServiceException("Interrupted..."));
+                return;
+            }
+            try {
+                cbak.send(req);
+            } catch (Exception e) {
+                cbak.sendError(new WebServiceException(e));
+            }
+        }
     }
 
 }
