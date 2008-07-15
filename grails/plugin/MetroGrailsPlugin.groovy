f886d334601112e2f8d9f02158284823bdc351d4
Martin Grebac <martin.grebac@oracle.com>
2008-07-15 15:42:02 +0000

    Fixed problem with services dependency
    svn path=/trunk/; revision=690
    
    
    Former-commit-id: a088d7ef7bde4a8d44a911f4e4fcf3b1aa343b50

--- grails/plugin/MetroGrailsPlugin.groovy
+++ grails/plugin/MetroGrailsPlugin.groovy
@@ -41,7 +41,7 @@ import groovy.xml.dom.DOMCategory
 import groovy.xml.StreamingMarkupBuilder
 
 class MetroGrailsPlugin {
-    def dependsOn = [services: '1.0.1']
+//    def dependsOn = []
 	
     def author = "Martin Grebac"
     def authorEmail = "martin.grebac@sun.com"
@@ -51,7 +51,7 @@ METRO plugin allows applications developed with Grails framework to expose servi
 classes as SOAP Web Services. It uses the SOAP implementation from METRO (includes JAX-WS).
 '''
     def documentation = "http://jax-ws-commons.dev.java.net/grails/"
-    def version = '1.0'
+    def version = '1.0.1'
     def loadAfter = ['services']    
     def watchedResources = ["file:./grails-app/services/**/*Service.groovy",
                             "file:./plugins/*/grails-app/services/**/*Service.groovy",
