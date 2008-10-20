7721090f8d98c611895c055eb377d24273274826
jitendra kotamraju <jitu@java.net>
2008-10-20 22:45:33 +0000

    Adding copyright header, some javadoc
    svn path=/trunk/; revision=722
    
    
    Former-commit-id: b2e1f2d74a4c7cc84277ce4f93b5f74bf4143522

--- transport/grizzly-lwhs-transport/jaxws-grizzly-lwhs/src/main/java/org/jvnet/jax_ws_commons/transport/grizzly_lwhs/server/LWHSProvider.java
+++ transport/grizzly-lwhs-transport/jaxws-grizzly-lwhs/src/main/java/org/jvnet/jax_ws_commons/transport/grizzly_lwhs/server/LWHSProvider.java
@@ -35,30 +35,25 @@
  */
 package org.jvnet.jax_ws_commons.transport.grizzly_lwhs.server;
 
-import com.sun.grizzly.tcp.http11.GrizzlyAdapter;
-import com.sun.grizzly.tcp.http11.GrizzlyRequest;
-import com.sun.grizzly.tcp.http11.GrizzlyResponse;
-import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.spi.HttpServerProvider;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsServer;
 
+import java.net.InetSocketAddress;
 import java.io.IOException;
 
 /**
+ * A provider for light weight http server. This provider is implemented using
+ * grizzly.
+ *
  * @author Jitendra Kotamraju
  */
-class LWHSAdapter extends GrizzlyAdapter {
-    private final LWHSContext lwhsContext;
-
-    LWHSAdapter(LWHSContext lwhsContext) {
-        this.lwhsContext = lwhsContext;
+public class LWHSProvider extends HttpServerProvider {
+    public HttpServer createHttpServer(InetSocketAddress addr, int backlog) throws IOException {
+        return new LWHSServer(addr, backlog);
     }
 
-    public void service(GrizzlyRequest request, GrizzlyResponse response) {
-        HttpHandler handler = lwhsContext.getHandler();
-        LWHSExchange exchange = new LWHSExchange(request, response);
-        try {
-            handler.handle(exchange);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public HttpsServer createHttpsServer(InetSocketAddress addr, int backlog) throws IOException {
+        throw new UnsupportedOperationException();
     }
 }
