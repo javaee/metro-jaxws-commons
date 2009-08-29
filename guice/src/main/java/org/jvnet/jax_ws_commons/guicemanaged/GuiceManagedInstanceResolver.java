b0fcd3fea9de788910eec04b97e6e9908a3da710
marcuseriksson <marcuseriksson@localhost>
2009-08-29 19:21:27 +0000

    fixes by ronak patel - support jsr250 and real singletons
    svn path=/trunk/; revision=854
    
    
    Former-commit-id: c9ccb00edcfaecb3617576fad569fb7357770bb9

--- guice/src/main/java/org/jvnet/jax_ws_commons/guicemanaged/GuiceManagedInstanceResolver.java
+++ guice/src/main/java/org/jvnet/jax_ws_commons/guicemanaged/GuiceManagedInstanceResolver.java
@@ -1,96 +1,133 @@
 package org.jvnet.jax_ws_commons.guicemanaged;
 
-import java.util.List;
-import java.util.ArrayList;
-import com.sun.xml.ws.server.AbstractMultiInstanceResolver;
-import com.sun.xml.ws.api.message.Packet;
-import com.sun.xml.ws.api.server.WSWebServiceContext;
-import com.sun.xml.ws.api.server.WSEndpoint;
-import com.sun.xml.ws.api.server.ResourceInjector;
-import com.sun.istack.NotNull;
-import com.google.inject.Injector;
-import com.google.inject.Guice;
-import com.google.inject.Module;
 import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.sun.istack.NotNull;
+import com.sun.xml.ws.api.message.Packet;
+import com.sun.xml.ws.api.server.ResourceInjector;
+import com.sun.xml.ws.api.server.WSEndpoint;
+import com.sun.xml.ws.api.server.WSWebServiceContext;
+import com.sun.xml.ws.server.AbstractMultiInstanceResolver;
+import org.guiceyfruit.Injectors;
+import org.guiceyfruit.support.CloseFailedException;
 
 import javax.xml.ws.WebServiceContext;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The instance resolver
- *
+ * <p/>
  * Looks at the endpoint class and gets the annotation in order to know what
  * guice module to use when injecting the dependencies into the endpoint.
- * 
  *
  * @author Marcus Eriksson, krummas@gmail.com
  * @since Nov 4, 2008
  */
 public class GuiceManagedInstanceResolver<T> extends AbstractMultiInstanceResolver<T> {
-    private Injector injector = null;
+
+    private static Injector injector;
+
     private ResourceInjector resourceInjector;
+
     private WSWebServiceContext webServiceContext;
-    public GuiceManagedInstanceResolver(@NotNull Class<T> clazz) throws IllegalAccessException, InstantiationException
-    {
+
+    public GuiceManagedInstanceResolver(@NotNull final Class<T> clazz)
+            throws IllegalAccessException, InstantiationException {
         super(clazz);
     }
 
-    /**
-     * Let guice create the instance
-     *
-     * the {@code create()} method in {@code AbstractMultiInstanceResolver} simply returns {@code clazz.newInstance()}
-     * so no magic happens there.
-     *
-     * If the endpoint is declared as singleton, the same instance will be returned every time.
-     *
-     * @param packet
-     * @return
-     */
+    /* (non-Javadoc)
+      * @see com.sun.xml.ws.api.server.InstanceResolver#dispose()
+      */
     @Override
-    public T resolve(@NotNull Packet packet) {
-        T instance = injector.getInstance(this.clazz);
-        resourceInjector.inject(webServiceContext,instance);
-        return instance;
-    }
+    public void dispose() {
 
-    /**
-     * save the web service context instance
-     * @param wsc
-     * @param endpoint
-     */
-
-    @Override
-    public void start(WSWebServiceContext wsc, WSEndpoint endpoint) {
-        super.start(wsc,endpoint);
-        resourceInjector = getResourceInjector(endpoint);
-        webServiceContext=wsc;
         try {
-            injector = getInjector();
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Illegal access in the endpoint",e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException("Could not instantiate the endpoint",e);
+            Injectors.close(GuiceManagedInstanceResolver.injector);
+        } catch (CloseFailedException e) {
+            //if context closing failed, create a hard fail exception
+            throw new RuntimeException(e);
         }
     }
 
     /**
      * Create an injector, adds a module for the web service context instance
-     * 
+     *
      * @return
      * @throws IllegalAccessException
      * @throws InstantiationException
      */
     private Injector getInjector() throws IllegalAccessException, InstantiationException {
-        List<Module> moduleInstances = new ArrayList<Module>();
-        Class<? extends Module>[] moduleClasses = clazz.getAnnotation(GuiceManaged.class).module();
-        for(Class<? extends Module> moduleClass : moduleClasses)
-        {
+
+        final List<Module> moduleInstances = new ArrayList<Module>();
+        final Class<? extends Module>[] moduleClasses = this.clazz.getAnnotation(GuiceManaged.class).module();
+
+        for (final Class<? extends Module> moduleClass : moduleClasses) {
             moduleInstances.add(moduleClass.newInstance());
         }
+
         moduleInstances.add(new AbstractModule() {
+
+            /* (non-Javadoc)
+                * @see com.google.inject.AbstractModule#configure()
+                */
+            @Override
             protected void configure() {
-                bind(WebServiceContext.class).toInstance(webServiceContext);
+                this.bind(WebServiceContext.class).toInstance(GuiceManagedInstanceResolver.this.webServiceContext);
             }
         });
+
         return Guice.createInjector(moduleInstances);
     }
+
+    /**
+     * Let guice create the instance
+     * <p/>
+     * the {@code create()} method in {@code AbstractMultiInstanceResolver}
+     * simply returns {@code clazz.newInstance()} so no magic happens there.
+     * <p/>
+     * If the endpoint is declared as singleton, the same instance will be
+     * returned every time.
+     *
+     * @param packet
+     * @return
+     */
+    @Override
+    public T resolve(@NotNull final Packet packet) {
+        final T instance = GuiceManagedInstanceResolver.injector.getInstance(this.clazz);
+        this.resourceInjector.inject(this.webServiceContext, instance);
+        return instance;
+    }
+
+    /**
+     * save the web service context instance
+     *
+     * @param wsc
+     * @param endpoint
+     */
+    @Override
+    public void start(final WSWebServiceContext wsc, final WSEndpoint endpoint) {
+        super.start(wsc, endpoint);
+
+        this.resourceInjector = GuiceManagedInstanceResolver.getResourceInjector(endpoint);
+        this.webServiceContext = wsc;
+
+        try {
+            //use double checked locking to ensure as close to a singleton instance as possible without static initialization
+            if (GuiceManagedInstanceResolver.injector == null) {
+                synchronized (GuiceManagedInstanceResolver.class) {
+                    if (GuiceManagedInstanceResolver.injector == null) {
+                        GuiceManagedInstanceResolver.injector = this.getInjector();
+                    }
+                }
+            }
+        } catch (final IllegalAccessException e) {
+            throw new RuntimeException("Illegal access in the endpoint", e);
+        } catch (final InstantiationException e) {
+            throw new RuntimeException("Could not instantiate the endpoint", e);
+		}
+	}
 }
